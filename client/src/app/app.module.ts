33d7f5e6654644a3a5cf4d6f22f4357aa1023ab0
€ŗïk Z <ezgo1010@hotmail.com>
2017-11-08 22:46:33 -0500

    Init server

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
+
+import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { RegisterComponent } from './register/register.component';
