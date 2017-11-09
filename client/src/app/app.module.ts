f737bcc38e70bd7c082cc76ed0b568c7599fa901
€ŗïk Z <ezgo1010@hotmail.com>
2017-11-08 21:05:38 -0500

    Init footer

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
